d29b8130d224d61b787133c6ab996f64d4d67274
Danny Zumwalt <dannyzumwalt@gmail.com>
2020-10-14 09:00:04 -0500

    add results parse script

---
+++ results.bash
@@ -0,0 +1,29 @@
+[dz1317@uvsaatsapp01 digDns]$ cat results.bash
+# get dns dig results and format.
+ 
+# dz1317@att.com
+# 10/08/2020
+ 
+if [[ $1 == "" ]]; then
+  echo "USAGE: Enter file name to prepare results for (outfile from your dig test)"
+  echo "example: ./results.bash my_out_file"
+  echo
+  exit
+else
+  file=$1
+fi
+ 
+#array of DNS servers to include in test - you should not need to edit this
+nameArray=(8.8.8.8 1.1.1.1 68.94.156.9 68.94.157.9 68.94.156.8 68.94.157.8)
+ 
+for name in ${nameArray[@]}; do
+  echo -n "$name - "
+  cycles=`grep ">> @${name} " $file | wc -l`
+  [[ $cycles -eq 0 ]] && echo " ... no cycles found. Still running? " && exit
+  timeouts=`grep -B 3 "connection timed out" $file | grep $name | wc -l`
+  echo -n "$timeouts timeouts "
+  p=`echo "scale=2; 100 * $timeouts / $cycles" | bc`
+  echo -n "(${p}% failure rate)"
+  echo " - [ $cycles cycles ]"
+done
+ 
