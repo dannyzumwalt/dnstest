c00a6ae9faa7e625947448ea46b53bdc84f2cc77
Danny Zumwalt <dannyzumwalt@gmail.com>
2020-10-14 14:45:09 -0500

    fix path to nameservers.bash

--- scripts/results.bash
+++ scripts/results.bash
@@ -15,7 +15,7 @@ else
 fi
  
 #array of DNS servers to include in test - you should not need to edit this
-source ${dir}/nameservers.bash
+source ${dir}/scripts/nameservers.bash
 #nameArray=(8.8.8.8 1.1.1.1 68.94.156.9 68.94.157.9 68.94.156.8 68.94.157.8)
  
 for name in ${nameArray[@]}; do
