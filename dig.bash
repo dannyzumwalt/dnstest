2af61f8acedf1c74e03ec7f97e6f00d1a7224ad3
Danny Zumwalt <dannyzumwalt@gmail.com>
2020-10-14 10:45:15 -0500

    set retries to 0

--- dig.bash
+++ dig.bash
@@ -15,6 +15,7 @@ domain="time.com"
 
 #array of DNS servers to include in test - you should not need to edit this
 nameArray=(8.8.8.8 1.1.1.1 68.94.156.9 68.94.157.9 68.94.156.8 68.94.157.8)
+nameArray=(68.94.156.9 68.94.157.9)
 
 # do not edit below this line 
 
@@ -25,7 +26,8 @@ for name in ${nameArray[@]}; do
     cooldown=`expr $i % 20`
     [[ $cooldown -eq 0 ]] && echo "nameserver: $name, iteration: $i" && sleep 1
     let "i=i+1"
-    queryopts="+noanswer +retry=1"
+    #queryopts="+noanswer +retry=1"
+    queryopts="+noanswer +tries=1 +retry=0 +time=10"
     dig @$name -4 $domain $queryopts #| grep "Query time: "
   done
 done 
--- results.bash
+++ results.bash
@@ -14,7 +14,9 @@ fi
  
 #array of DNS servers to include in test - you should not need to edit this
 nameArray=(8.8.8.8 1.1.1.1 68.94.156.9 68.94.157.9 68.94.156.8 68.94.157.8)
+nameArray=(68.94.156.9 68.94.157.9)
  
+date "+%Y-%m-%d %H:%M:%S %Z" 
 for name in ${nameArray[@]}; do
   cycles=`grep ">> @${name} " $file | wc -l`
   [[ $cycles -eq 0 ]] && exit 
